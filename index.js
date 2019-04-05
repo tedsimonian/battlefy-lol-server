46aa815b12fae2716c17b1a46dd30aec8ff41d0f
Ted <tedsimonian@gmail.com>
2019-04-04 19:09:41 -0700

    refactored cors

--- index.js
+++ index.js
@@ -8,22 +8,19 @@ const allowedOrigins = [process.env.CLIENT_ORIGIN, process.env.SERVER_ORIGIN];
 
 const app = express();
 
-app.use(
-  cors({
-    origin: function(origin, callback) {
-      // allow requests with no origin
-      if (!origin) {
-        return callback(null, true);
-      }
-      // do not allow access if site isn't apart of allowed origins
-      if (allowedOrigins.indexOf(origin) === -1) {
-        return callback(new Error(`Invalid origin: ${origin}`), false);
-      }
-
+const corsOptions = {
+  origin: function(origin, callback) {
+    // allow requests from whitelisted origins or any REST or server-to-server requests
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       return callback(null, true);
+    } else {
+      // do not allow access if site isn't apart of allowed origins
+      return callback(new Error(`Invalid origin: ${origin}`));
     }
-  })
-);
+  }
+};
+
+app.use(cors(corsOptions));
 
 // api router
 app.use("/api", apiRoutes);
